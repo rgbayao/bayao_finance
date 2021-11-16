73c26c504b6e3e70ecc0165e951093744bedcff2
rgbayao <rgbayao@gmail.com>
2021-11-16 20:03:33 -0300

    change name to persistence

--- bayao_finance/__init__.py
+++ bayao_finance/__init__.py
@@ -1,3 +1,4 @@
 from bayao_finance.stockseries import StockSeries
 from bayao_finance.stockframe import StockFrame
 import bayao_finance.indicators as indicators
+from bayao_finance.persistence import StockManipulator
